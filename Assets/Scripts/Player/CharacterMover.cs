2255b27c3454b671556f072ad5350cd4abab77b3
moriiiiiiiiiiii12 <wssdozh@gmail.com>
2025-10-22 10:54:07 +0300

    import indicators

--- Assets/Scripts/Player/CharacterMover.cs
+++ Assets/Scripts/Player/CharacterMover.cs
@@ -1,24 +1,17 @@
 using UnityEngine;
 
 
-[RequireComponent(typeof(CapsuleCollider))]
 public class CharacterMover : MonoBehaviour
 {
     [Header("Зависимости")]
     [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private ForceMode _forceMode;
 
     [Header("Настройки")]
     [SerializeField] private float _speed = 3f;
     [SerializeField] private float _speedSprint = 6f;
-    [SerializeField] private float _jumpForce = 5f;
-    [SerializeField] private float _rayLengthGround = 0.2f;
-    [SerializeField] private LayerMask _groundLayer = 1;
 
     private Vector3 _moveDirection;
     private bool _isSprinting;
-
-    private bool IsGrounded => Physics.Raycast(transform.position, Vector3.down, _rayLengthGround, _groundLayer);
 
     private void FixedUpdate()
     {
@@ -35,31 +28,14 @@ public class CharacterMover : MonoBehaviour
         _isSprinting = sprinting;
     }
 
-    public void OnJump()
-    {
-        if (IsGrounded)
-        {
-            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
-        }
-    }
-
     private void Move()
-    {        
+    {
         if (_moveDirection.sqrMagnitude < 0.01f)
-        {
-            // _rigidbody.linearVelocity = new Vector3(0f, _rigidbody.linearVelocity.y, 0f); 
             return;
-        }
 
         float speed = _isSprinting ? _speedSprint : _speed;
         Vector3 targetVelocity = _moveDirection.normalized * speed;
 
         _rigidbody.linearVelocity = new Vector3(targetVelocity.x, _rigidbody.linearVelocity.y, targetVelocity.z);
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * _rayLengthGround);
     }
 }
