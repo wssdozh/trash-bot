273c826e09f28bdb07d1ddee9cd4ef5803beaa2d
moriiiiiiiiiiii12 <wssdozh@gmail.com>
2026-01-19 09:31:00 +0300

    edit character mover

--- Assets/Scripts/Player/Movement/CharacterMover.cs
+++ Assets/Scripts/Player/Movement/CharacterMover.cs
@@ -9,12 +9,19 @@ public class CharacterMover : MonoBehaviour
     [SerializeField] private float _speed = 3f;
     [SerializeField] private float _speedSprint = 6f;
 
+    [Header("Стена")]
+    [SerializeField] private float _wallNormalMaxY = 0.2f;
+
     private Vector3 _moveDirection;
     private bool _isSprinting;
+
+    private Vector3 _wallNormalSum;
+    private int _wallNormalCount;
 
     private void FixedUpdate()
     {
         Move();
+        ClearWallContacts();
     }
 
     public void OnMove(Vector2 input)
@@ -27,13 +34,64 @@ public class CharacterMover : MonoBehaviour
         _isSprinting = sprinting;
     }
 
+    private void OnCollisionStay(Collision collision)
+    {
+        int contactCount = collision.contactCount;
+        int contactIndex = 0;
+
+        while (contactIndex < contactCount)
+        {
+            ContactPoint contactPoint = collision.GetContact(contactIndex);
+            Vector3 normal = contactPoint.normal;
+
+            if (normal.y <= _wallNormalMaxY)
+            {
+                _wallNormalSum += normal;
+                _wallNormalCount += 1;
+            }
+
+            contactIndex += 1;
+        }
+    }
+
     private void Move()
     {
-        float speed = _isSprinting ? _speedSprint : _speed;
+        float speed = _speed;
+
+        if (_isSprinting == true)
+        {
+            speed = _speedSprint;
+        }
 
         Vector3 clampedMoveDirection = Vector3.ClampMagnitude(_moveDirection, 1f);
         Vector3 targetVelocity = clampedMoveDirection * speed;
 
-        _rigidbody.linearVelocity = new Vector3(targetVelocity.x, _rigidbody.linearVelocity.y, targetVelocity.z);
+        Vector3 adjustedVelocity = targetVelocity;
+
+        if (_wallNormalCount > 0)
+        {
+            Vector3 averageWallNormal = _wallNormalSum / (float)_wallNormalCount;
+
+            if (averageWallNormal.sqrMagnitude > 0.0001f)
+            {
+                averageWallNormal.Normalize();
+
+                float velocityAlongNormal = Vector3.Dot(targetVelocity, averageWallNormal);
+
+                if (velocityAlongNormal < 0f)
+                {
+                    adjustedVelocity = targetVelocity - (averageWallNormal * velocityAlongNormal);
+                }
+            }
+        }
+
+        float currentVerticalVelocity = _rigidbody.linearVelocity.y;
+        _rigidbody.linearVelocity = new Vector3(adjustedVelocity.x, currentVerticalVelocity, adjustedVelocity.z);
+    }
+
+    private void ClearWallContacts()
+    {
+        _wallNormalSum = Vector3.zero;
+        _wallNormalCount = 0;
     }
 }
