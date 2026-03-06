4a2f352d243b803d81c66dc5dbc6aef85a0d7d82
moriiiiiiiiiiii12 <wssdozh@gmail.com>
2026-03-06 14:01:22 +0300

    update

--- Assets/Scripts/Characters/Turret/Turret.cs
+++ Assets/Scripts/Characters/Turret/Turret.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
+    private const string LogPrefix = "[TurretLife]";
+
     [Header("Зависимости")]
     [SerializeField] private TargetVision _targetVision;
     [SerializeField] private TargetRotator _targetRotator;
@@ -12,20 +14,40 @@ public class Turret : MonoBehaviour
     [Header("Настройки")]
     [SerializeField] private float _fireDelaySeconds = 0.25f;
 
+    private Health _health;
     private Coroutine _fireDelayCoroutine;
+
+    private void Awake()
+    {
+        _health = GetComponent<Health>();
+
+        if (_health == null)
+        {
+            throw new MissingReferenceException(nameof(_health));
+        }
+    }
 
     private void OnEnable()
     {
+        _health.Ended += OnHealthEnded;
         _targetVision.TargetDetected += OnTargetFound;
         _targetVision.TargetCleared += OnTargetLost;
     }
 
     private void OnDisable()
     {
+        LogLifeEvent(nameof(OnDisable));
+
+        _health.Ended -= OnHealthEnded;
         _targetVision.TargetDetected -= OnTargetFound;
         _targetVision.TargetCleared -= OnTargetLost;
 
         StopFireDelay();
+    }
+
+    private void OnDestroy()
+    {
+        LogLifeEvent(nameof(OnDestroy));
     }
 
     private void Start()
@@ -35,7 +57,6 @@ public class Turret : MonoBehaviour
 
     private void Update()
     {
-
         if (_targetRotator.enabled == false)
         {
             _fireExecutor.ClearAimPoint();
@@ -60,12 +81,16 @@ public class Turret : MonoBehaviour
         }
 
         _fireExecutor.SetAimPoint(currentTarget.position);
-
     }
 
     private void OnTargetFound()
     {
         SetTrackingState();
+    }
+
+    private void OnHealthEnded()
+    {
+        LogLifeEvent(nameof(OnHealthEnded));
     }
 
     private void OnTargetLost()
@@ -99,7 +124,6 @@ public class Turret : MonoBehaviour
 
     private void StopFireDelay()
     {
-
         if (_fireDelayCoroutine == null)
         {
             return;
@@ -111,7 +135,6 @@ public class Turret : MonoBehaviour
 
     private IEnumerator FireWithDelayCoroutine()
     {
-
         if (_fireDelaySeconds > 0f)
         {
             yield return new WaitForSeconds(_fireDelaySeconds);
@@ -119,5 +142,40 @@ public class Turret : MonoBehaviour
 
         _fireDelayCoroutine = null;
         _fireExecutor.StartFiring();
+    }
+
+    private void LogLifeEvent(string eventName)
+    {
+        string healthText = "null";
+
+        if (_health != null)
+        {
+            healthText = _health.Value + "/" + _health.MaxValue;
+        }
+
+        string message =
+            LogPrefix +
+            " " +
+            name +
+            " event:" +
+            eventName +
+            " frame:" +
+            Time.frameCount +
+            " scene:" +
+            gameObject.scene.name +
+            " activeSelf:" +
+            gameObject.activeSelf +
+            " activeInHierarchy:" +
+            gameObject.activeInHierarchy +
+            " enabled:" +
+            enabled +
+            " health:" +
+            healthText +
+            " position:" +
+            transform.position +
+            "\n" +
+            StackTraceUtility.ExtractStackTrace();
+
+        Debug.LogWarning(message, this);
     }
 }
